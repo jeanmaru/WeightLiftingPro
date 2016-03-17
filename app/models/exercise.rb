504fe7d7c2acf12911b9421d5b69c1bb3eabf335
Jess Rudolph <jam.rudolph@gmail.com>
2016-03-17 12:53:11 -0400

    Delete Exercise Deletes Associated Exercise Logs

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,7 +2,7 @@ class Exercise < ActiveRecord::Base
   belongs_to :sample_exercise
   belongs_to :user
   belongs_to :routine
-  has_many :exercise_entries
+  has_many :exercise_entries,  dependent: :destroy
 
   validates_presence_of :sample_exercise_id
   validates_uniqueness_of :sample_exercise_id, scope: [:user_id]
