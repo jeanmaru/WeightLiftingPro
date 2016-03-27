ee0e5c57d508e1e428013e4fbcbdb56dba1b3826
Jess Rudolph <jam.rudolph@gmail.com>
2016-03-27 14:47:45 -0400

    Monthly Report with Feedback Goals Part 1

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -7,4 +7,24 @@ module ReportsHelper
     Date.today - 30.days
   end
 
+  def bw_85
+    current_user.body_weight * 0.85
+  end
+  
+  def bw_200
+    current_user.body_weight * 2.00
+  end
+  
+  def bw_250
+    current_user.body_weight * 2.50
+  end
+
+
+  def bw_150
+    current_user.body_weight * 1.50
+  end
+  
+  
+
+  
 end
