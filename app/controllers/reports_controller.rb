cb6febefbafddb02a962d72dd95ff43ccd7e3c0c
Jess Rudolph <jam.rudolph@gmail.com>
2016-04-07 18:52:07 -0400

    Report to PDF DL | No Errors

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,28 @@
 class ReportsController < ApplicationController
   def download_pdf
-    html = render_to_string(:action => 'reports/exercise_entry_month')
+    html = render_to_string(:action => '/exercise_entry_month')
     kit = PDFKit.new(html, :page_size => 'Letter')
-    
-    
-# Get an inline PDF
+    # file = kit.to_file('/downloads')
+
+
+
+    # Get an inline PDF
 #     pdf = kit.to_pdf
 
     # send_data(kit.to_pdf)
   end
+
+  # def show
+  #   @report = Report.find(params[:id])
+  # 
+  #   kit       = PDFKit.new render_to_string(@report) # pass any options that the
+  #   pdf_file  = kit.to_file '/some/file/path.pdf'    # renderer needs here (same
+  #   # options as `render`).
+  #   # and if you still want to send it to the browser...
+  #   respond_to do |format|
+  #     format.pdf { render :file => pdf_file.path }
+  #   end
+  # end
 
 #   kit = PDFKit.new(html, :page_size => 'Letter')
 #   kit.stylesheets << '/path/to/css/file'
