4eb93d51810c99250cffcbd428529107d478de4b
Jess Rudolph <jam.rudolph@gmail.com>
2016-04-09 14:05:46 -0400

    Report to PDF DL | Before Executing

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,7 +4,15 @@ class ReportsController < ApplicationController
     pdf = WickedPdf.new.pdf_from_string(html)
 
     send_data(pdf,
+              :layout => 'pdf.html.erb',
               :filename => "My_Monthly_Exercise_Report.pdf",
+              :page_size => 'Letter',
+              :margin => {
+                  :top      => '0.5in',
+                  :bottom   => '1in',
+                  :left     => '0in',
+                  :right    => '0in'
+              },
               :disposition => 'attachment')
 
   end
