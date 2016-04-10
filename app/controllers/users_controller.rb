577bec0754d6e9b8ed0f579dc0b4469837c51f4e
Jess Rudolph <jam.rudolph@gmail.com>
2016-04-10 12:58:33 -0400

    Users Form & Devise Notices | Edit User Admin View

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,25 +25,12 @@ class UsersController < ApplicationController
   end
   
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User Profile Successfully Updated!' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to registration_path(@user)
+    else
+      render :edit
     end
   end
-
-  # 
-  # def update
-  #   if @user.update(user_params)
-  #     redirect_to registration_path(@user)
-  #   else
-  #     render :edit
-  #   end
-  # end
   
   
   def destroy
