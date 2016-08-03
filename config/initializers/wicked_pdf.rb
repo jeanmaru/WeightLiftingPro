0dcdfc8cbe29d81802f50cf147232a070bb1bc36
Jess Rudolph <jam.rudolph@gmail.com>
2016-08-03 19:47:27 -0400

    Wicked PDF for Heroku Part 2

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,17 +1,17 @@
-if Rails.env.production?
-wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
-else
-wkhtmltopdf_path = "/usr/local/bin/wkhtmltopdf"
-end
-
-WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
-
-
-
-# if defined?(WickedPdf)
-# 
-# WickedPdf.config = {
-#     :exe_path => '/usr/local/bin/wkhtmltopdf'
-# }
+# if Rails.env.production?
+# wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+# else
+# wkhtmltopdf_path = "/usr/local/bin/wkhtmltopdf"
 # end
+# 
+# WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
+# 
+
+
+if defined?(WickedPdf)
+
+WickedPdf.config = {
+    :exe_path => '/bin/wkhtmltopdf-amd64'
+}
+end
 
